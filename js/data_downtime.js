eabd2fcba603a5928216d519e105783f1d18cf32
BRYN <bryn4444@gmail.com>
2019-06-04 01:53:03 -0600

    reverting to basic currency

--- js/data_downtime.js
+++ js/data_downtime.js
@@ -46,7 +46,7 @@ data_rest = [
         bullets: [
             "Inns allow players to <b>long rest</b> safe from <b>outdoor threats</b> like animals or monsters, and <b>weather</b> like rain, snow, or sandstorms. They may also allow you to choose a meal type to fufil your <i>hunger</i> at an <b>extra cost</b>.",
             "Inns vary in availability between towns or cities, and each of them is a different quality of experience.",
-            "<table><tr><th style='text-align:left;'>Quality</th><th></th><th></th><th style='text-align:left;'>Price/Rest</th><th></th><th></th><th style='text-align:left;'>Meals to Purchase (Extra)</th></tr><tr><td>Meager</td><td></td><td></td><td>0.07g</td><td></td><td></td><td>---</td></tr><tr><td>Ordinary</td><td></td><td></td><td>0.5g</td><td></td><td></td><td>Small (0.05g)</td></tr><tr><td>Hospitable</td><td></td><td></td><td>1g</td><td></td><td></td><td>Normal (0.5g), Hearty (1g)</td></tr><tr><td>Expensive</td><td></td><td></td><td>2g</td><td></td><td></td><td>Hearty (1g), Great (5g), Banquet (10g)</td></tr></table>",
+            "<table><tr><th style='text-align:left;'>Quality</th><th></th><th></th><th style='text-align:left;'>Price/Rest</th><th></th><th></th><th style='text-align:left;'>Meals to Purchase (Extra)</th></tr><tr><td>Meager</td><td></td><td></td><td>7 cp</td><td></td><td></td><td>---</td></tr><tr><td>Ordinary</td><td></td><td></td><td>5 sp</td><td></td><td></td><td>Small (5 cp)</td></tr><tr><td>Hospitable</td><td></td><td></td><td>1 gp</td><td></td><td></td><td>Normal (5 sp), Hearty (1 gp)</td></tr><tr><td>Expensive</td><td></td><td></td><td>2 gp</td><td></td><td></td><td>Hearty (1 gp), Great (5 gp), Banquet (10 gp)</td></tr></table>",
         ]
     }
 ]
@@ -63,7 +63,7 @@ data_downtime = [
             "Before your next quest, <b>choose a lifestyle from the table below</b>, unless your downtime activity has a predetermined lifestyle expenses and cost. Calculate the cost of your chosen lifestyle by <b>multiplying the price by a period of days</b> determined by the DM.",
             "Your lifestyle might change from one period to the next, based on the funds you have at your disposal, or you might maintain the same lifestyle throughout your character's career.",
             "Your lifestyle choice can have consequences or benefits. Maintaining a wealthy lifestyle might help you make contacts with the rich and powerful, though you run the risk of attracting thieves. Likewise, living frugally might help you avoid criminals, but you can be seen as a peon to the rich.",
-            "<table><tr><th style='text-align:left'>Lifestyle</th><th></th><th></th><th style='text-align:left'>Price/Day</th><th></th><th></th><th></th><th></th><th style='text-align:left'>Lifestyle</th><th></th><th></th><th style='text-align:left'>Price/Day</th></tr><tr><td>Wretched</td><td></td><td></td><td>---</td><td></td><td></td><td></td><td></td><td>Comfortable</td><td></td><td></td><td>2g</td></tr><tr><td>Squalid</td><td></td><td></td><td>0.1g</td><td></td><td></td><td></td><td></td><td>Wealthy</td><td></td><td></td><td>4g</td></tr><tr><td>Poor</td><td></td><td></td><td>0.2g</td><td></td><td></td><td></td><td></td><td>Aristocratic</td><td></td><td></td><td>10g minimum</td></tr><tr><td>Modest</td><td></td><td></td><td>1g</td><td></td><td></td><td></td><td></td><td colspan='4'><i>Descriptions in PHB, pg. 158.</i></td></tr></table>"
+            "<table><tr><th style='text-align:left'>Lifestyle</th><th></th><th></th><th style='text-align:left'>Price/Day</th><th></th><th></th><th></th><th></th><th style='text-align:left'>Lifestyle</th><th></th><th></th><th style='text-align:left'>Price/Day</th></tr><tr><td>Wretched</td><td></td><td></td><td>---</td><td></td><td></td><td></td><td></td><td>Comfortable</td><td></td><td></td><td>2 gp</td></tr><tr><td>Squalid</td><td></td><td></td><td>1 sp</td><td></td><td></td><td></td><td></td><td>Wealthy</td><td></td><td></td><td>4 gp</td></tr><tr><td>Poor</td><td></td><td></td><td>2 sp</td><td></td><td></td><td></td><td></td><td>Aristocratic</td><td></td><td></td><td>10 gp minimum</td></tr><tr><td>Modest</td><td></td><td></td><td>1 gp</td><td></td><td></td><td></td><td></td><td colspan='4'><i>Descriptions in PHB, pg. 158.</i></td></tr></table>"
         ]
     },
     {
@@ -75,10 +75,10 @@ data_downtime = [
         bullets: [
             "You must be <b>proficient with tools</b> related to the object you are trying to create (typically <i>artisan's tools</i>).",
             "You might also need access to <b>special materials or locations</b> necessary to create it. For example, someone proficient with <i>smith's tools</i> needs a <i>forge</i> in order to craft a sword or suit of armor.",
-            "For every day you spend crafting, you make one or more items with a <b>total market value &le;5g</b>, and expend raw materials worth half that much.",
-            "If something you want to craft has a market value >5g, you make progress every day in <b>5g increments</b> until you reach the market value of the item. For example, 1,500g armor would take 300 days to craft by yourself.",
-            "<b>Multiple characters</b> can combine their efforts toward the crafting of a single item, provided that the characters are <b>all in the same place and have proficiency with the requisite tools</b>. Each character contributes 5g worth of effort for every day spent helping to craft the item. For example, 1,500g armor would take three proficient characters 100 days to create, at a total cost of 750g.",
-            "While crafting, you can maintain a <b>modest lifestyle</b> without having to pay 1g per day, or a <b>comfortable lifestyle</b> at half the normal cost"
+            "For every day you spend crafting, you make one or more items with a <b>total market value &le;5 gp</b>, and expend raw materials worth half that much.",
+            "If something you want to craft has a market value >5 gp, you make progress every day in <b>5 gp increments</b> until you reach the market value of the item. For example, 1,500 gp armor would take 300 days to craft by yourself.",
+            "<b>Multiple characters</b> can combine their efforts toward the crafting of a single item, provided that the characters are <b>all in the same place and have proficiency with the requisite tools</b>. Each character contributes 5 gp worth of effort for every day spent helping to craft the item. For example, 1,500 gp armor would take three proficient characters 100 days to create, at a total cost of 750 gp.",
+            "While crafting, you can maintain a <b>modest lifestyle</b> without having to pay 1 gp per day, or a <b>comfortable lifestyle</b> at half the normal cost"
         ]
     },
     {
@@ -88,7 +88,7 @@ data_downtime = [
         description: "Work during your downtime to maintain a better lifestyle, or earn extra funds",
         reference: "PHB, pg. 187 (with slight homebrew modifications).",
         bullets: [
-            "Working between adventures allows you to maintain a <b>modest lifestyle</b> without having to pay 1g per day. This benefit lasts as long you continue to practice your profession.",
+            "Working between adventures allows you to maintain a <b>modest lifestyle</b> without having to pay 1 gp per day. This benefit lasts as long you continue to practice your profession.",
             "If you are a member of an organization that can provide gainful employment, such as a temple or a thieves' guild, you earn enough to support a <b>comfortable lifestyle</b> instead.",
             "If you have <b>proficiency in the Performance skill</b> and put your performance skill to use during your downtime, you earn enough to support a <b>wealthy lifestyle</b> instead.",
             "While working, you can choose instead to take the next lowest lifestyle and keep the funds you'd save for yourself."
@@ -105,7 +105,7 @@ data_downtime = [
             "When you begin your research, the DM determines <b>whether the information is available</b>, how many days of downtime it will take to find, and whether there are any restrictions on your research (such as needing to seek out a specific individual, tome, or location).",
             "The DM might also require you to make one or more <b>ability checks</b>, such as an Investigation check to find clues pointing toward the information you seek, or a Persuasion check to secure someone's aid.",
             "Once the above conditions are met, you learn the information if it is available.",
-            "For each day of research, you must <b>spend 1g</b> to cover your investigation expenses <b>in addition to your normal lifestyle expenses</b>."
+            "For each day of research, you must <b>spend 1 gp</b> to cover your investigation expenses <b>in addition to your normal lifestyle expenses</b>."
         ]
     },
     {
